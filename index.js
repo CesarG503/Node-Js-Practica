3ebf9b54f0f8da76ef7c7dcf69eae83807b6658d
CesarG503 <cesargaray06@gmail.com>
2025-01-13 14:51:49 -0600

    NODE JS prueba de ejecucion del javaScript

---
+++ index.js
@@ -0,0 +1,20 @@
+
+console.log("Hello World");
+
+let user_name = "John Doe";
+let age = 25;
+let adult = true;
+let points = 100;
+
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+
+if (age >= 18) {
+    console.log("You are an adult");
+}
