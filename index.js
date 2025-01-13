4277ddb1e4a0ed832f93f10010cd49119646e89f
CesarG503 <cesargaray06@gmail.com>
2025-01-13 14:58:23 -0600

    Niveles de acceso y diferencias con var let const

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ let adult = true;
 let points = 100;
 
 
+
+const names_1 = ["John", "Jane", "Jack"];
+let names_2 = ["John", "Jane", "Jack"];
+var names_3 = ["John", "Jane", "Jack"];
+
+
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 for (let index = 0; index < array.length; index++) {
