d5f878230aa35cfb3bce1f89c8de5e2dc7e08c65
CesarG503 <cesargaray06@gmail.com>
2025-02-01 01:11:03 -0600

    https Explicacion de las request

--- https.js
+++ https.js
@@ -1,16 +1,38 @@
 const http = require('http')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//creacion de un web Server
+//protocolo HTTP
 
+const hostname = '127.0.0.1';
+const port = 3000; //ver HTTP Port para saber que puerto usar 
+
+//funcion http para crear el servidor (manejar request(peticion), response)
+
+//call back
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+
+    console.log(req.url); //nos llegan dos la ruta en la que estamos y el icono 
+
+    if(req.url === '/')
+        {
+            res.write('pagina general');
+            return res.end();
+        }
+    
+    if(req.url=== '/about')
+        {
+            res.write('<h1>acerca de Node.js</h1>');
+            return res.end();
+        }
+
+
+    res.write('hola server\n');
+    res.end('Servidor corriendo...!\n');
 });
 
+//funcion para abrir el servidor que se este ejecutando constantemente 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server corriendo en  http://${hostname}:${port}/`);
 });
 
 
