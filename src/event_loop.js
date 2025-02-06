fb420f21f16002f4afbe728d93cf8a32e828fd41
CesarG503 <cesargaray06@gmail.com>
2025-02-05 23:50:04 -0600

    promise http con setimeout, para no generar bloqueos de codigos en servidores

--- src/event_loop.js
+++ src/event_loop.js
@@ -10,3 +10,44 @@ setTimeout(()=>
 
 // el codigo continua no espera a que termine la funcion setTimeout (Sync) 
 console.log('3')
+
+
+
+const http = require('http')
+
+const port = 3000;
+
+let server = http.createServer((req,res)=>
+    {
+        if(req.url === '/')
+            {
+                res.write('<h1>Mensaje de la ruta principal</h1>')
+                return res.end();
+            }
+        if(req.url === '/about')
+            {
+                res.write('<h1>Ruta about</h1>')
+                
+                setTimeout(()=>{
+                
+                //tarea bloking code (esta tarea bloquea la ejucion del servidor) (Hilo)
+                for (let index = 0; index < 100000; index++) {
+                    console.log(`numero generado: ${(Math.random() * index)}`)
+                } 
+                return res.end();
+                })
+
+
+
+            }
+        else
+        {
+            res.write('<h1>No encontrado</h1>')
+            return res.end();
+        }
+
+    });
+
+server.listen(port)
+
+console.log(`server abierto en el server ${port}`)
