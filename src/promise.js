6647a7c6fc3ae85d2b995d4a182fbc84b172d825
CesarG503 <cesargaray06@gmail.com>
2025-02-07 22:29:33 -0600

    promesa Asyncrona y manejo de errores de lectura

--- src/promise.js
+++ src/promise.js
@@ -20,17 +20,40 @@ GetText('./data/segundo.txt')
 
 async function ReadText() // todo lo que esta adentro de esta funcion async se ejeuta secuencialmente
 {   
+    try{
     const result = await GetText('./data/primero.txt')   
     const result2 = await GetText('./data/segundo.txt')  
     const result3 = await GetText('./data/tercero.txt')    
-
+      
     console.log(result)
     console.log(result2)
     console.log(result3)
+
+    throw new Error('Error creado')
+
+    }catch (error)
+    {
+
+    }
+
 }
 
 ReadText();
 
+async function ReadTextAsync() // Las operaciones dentro de Promise.all se ejecutan en paralelo
+{   
+    const [result, result2, result3] = await Promise.all([
+        GetText('./data/primero.txt'),
+        GetText('./data/segundo.txt'),
+        GetText('./data/tercero.txt')
+    ]);
+
+    console.log(result);
+    console.log(result2);
+    console.log(result3);
+}
+
+ReadTextAsync();
 
 
 
